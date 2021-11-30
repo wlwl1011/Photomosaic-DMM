1d9e1d4863b614e00e0eefb684cbc14bfe836a9b
CalifornaLuv <caifornialove.96@gmail.com>
2021-11-30 20:05:01 +0900

    add: main 페이지 레이아웃

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./font/font.css";
+import Main from "./page/main/Main";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
