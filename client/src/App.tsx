53f6e1a50ecfd2f41d036abf3c0cdab11062fbca
Lee <Lee@Lee-iMac.local>
2021-12-09 13:34:01 +0900

    add: Kakaomap 별점 기능

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./App.css";
 import "./font/font.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./page/main/Main";
 import Menu from "./page/menu/Menu";
 import Store from "./page/store/Store";
 import Mypage from "./page/mypage/Mypage";
 import Signup from "./components/signup/Signup";
-import Kakao_map from "./components/kakao_map/Kakao_map";
+import Star_select from "./components/star/star_select/Star_select";
 
 function App() {
   return (
     <>
       <div className="wrap">
-        <Menu />
+        <Store />
       </div>
     </>
   );
--- client/src/components/kakao_map/Kakao_map.tsx
+++ client/src/components/kakao_map/Kakao_map.tsx
@@ -8,6 +8,15 @@ declare global {
 }
 
 function Kakao_map() {
+  // const [width, setWidth] = useState<number>(0);
+
+  // let result: any = document.querySelector<HTMLElement>(".kakao_map_container");
+  // // 좌표 벗어날 경우 재설정 함수
+  // function setCenter() {
+  //   const moveLatLon = new window.kakao.maps.LatLng(37.54699, 127.09598);
+  //   map.setCenter(moveLatLon);
+  // }
+  // setCenter();
   useEffect(() => {
     let container = document.getElementById("map");
 
@@ -19,11 +28,23 @@ function Kakao_map() {
     let map = new window.kakao.maps.Map(container, options);
 
     console.log("loading kakaomap");
-    var markerPosition = new window.kakao.maps.LatLng(37.54699, 127.09598);
+
+    var imageSrc = "./maker.png", // 마커이미지의 주소입니다
+      imageSize = new window.kakao.maps.Size(70, 70), // 마커이미지의 크기입니다
+      imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new window.kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      ),
+      markerPosition = new window.kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
 
     // 마커를 생성합니다
     var marker = new window.kakao.maps.Marker({
       position: markerPosition,
+      image: markerImage, // 마커이미지 설정
     });
 
     // 마커가 지도 위에 표시되도록 설정합니다
