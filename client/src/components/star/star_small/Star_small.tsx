4a47dd6a17d6b0b26f234b8615bb41896c0fe56b
Lee <Lee@Lee-iMac.local>
2021-12-13 12:43:27 +0900

    add: 충돌 방지 작업

--- client/src/components/star/star_small/Star_small.tsx
+++ client/src/components/star/star_small/Star_small.tsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import "./Star_small.css";
 
 interface Iprops {
-    rating:number
+  rating: number;
 }
 
-function Star_avg(props : Iprops) {
+function Star_avg(props: Iprops) {
+  console.log("push 최신화할려고 생성");
   return (
     <>
       <div className="star_small-box">
