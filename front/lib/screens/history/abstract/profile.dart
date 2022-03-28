597daeb3ea3551a9a2f3902d386250e652132e31
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-03-29 01:13:02 +0900

    semi user page

--- front/lib/screens/history/abstract/profile.dart
+++ front/lib/screens/history/abstract/profile.dart
@@ -1,33 +1,83 @@
 import 'package:front/screens/history/abstract/profile/my_info.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Expanded(
-                flex: 4,
-                child: Stack(
-                  children: <Widget>[
-                    SafeArea(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            MyInfo(),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const MyInfo(),
+          Row(
+            children: [
+              SizedBox(width: Get.width * 0.02),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_1.jpeg"),
               ),
+              SizedBox(width: Get.width * 0.03),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_2.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.03),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_3.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.02),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(width: Get.width * 0.02),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_4.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.03),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_5.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.03),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_6.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.02),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(width: Get.width * 0.02),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_7.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.03),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_8.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.03),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: Get.height * 0.3,
+                child: Image.asset("assets/images/userImage_9.jpeg"),
+              ),
+              SizedBox(width: Get.width * 0.02),
             ],
           ),
         ],
--- front/lib/screens/history/abstract/profile/my_info.dart
+++ front/lib/screens/history/abstract/profile/my_info.dart
@@ -1,3 +1,4 @@
+import 'package:front/constants/color_constant.dart';
 import 'package:front/screens/history/abstract/profile/radial_progress.dart';
 import 'package:front/screens/history/abstract/profile/rounded_image.dart';
 
@@ -5,36 +6,36 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MyInfo extends StatelessWidget {
+  const MyInfo({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const RadialProgress(
-            width: 4,
-            goalCompleted: 0.9,
-            child: RoundedImage(
-              imagePath: "assets/images/profile.jpeg",
-              size: Size.fromWidth(70.0),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 40,
+        ),
+        const RadialProgress(
+          width: 4,
+          goalCompleted: 0.9,
+          child: RoundedImage(
+            imagePath: "assets/images/profile.jpeg",
+            size: Size.fromWidth(70.0),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              "Anne Grethe",
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                "Anne Grethe",
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
