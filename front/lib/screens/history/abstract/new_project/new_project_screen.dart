2814dc43b3c44bf4a0431d5572750c4330c031e6
eunha812 <0812keh@naver.com>
2022-03-29 01:37:28 +0900

    새 프로젝트 생성 개요 설정

---
+++ front/lib/screens/history/abstract/new_project/new_project_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewProjectScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(child: Text("새 프로젝트 창")),
+    );
+  }
+}
