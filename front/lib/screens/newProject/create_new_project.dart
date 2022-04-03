f0408eceb402e05cf832c7c32679f4160571e9e9
eunha812 <0812keh@naver.com>
2022-04-04 03:14:08 +0900

    create new project

--- front/lib/screens/newProject/create_new_project.dart
+++ front/lib/screens/newProject/create_new_project.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:front/constants/color_constant.dart';
 import 'package:front/screens/history/main/main_screen.dart';
 import 'package:get/get.dart';
 
-class CreateNewProject extends StatelessWidget {
-  const CreateNewProject({Key? key}) : super(key: key);
+class CreateNewProject extends StatefulWidget {
+  CreateNewProject({Key? key}) : super(key: key);
 
   @override
+  State<CreateNewProject> createState() => _CreateNewProjectState();
+}
+
+class _CreateNewProjectState extends State<CreateNewProject> {
+  var photomosaicImage = Get.arguments;
+
   Widget _createNewProjectBodyWidget() {
-    return Text("포토모자이크 생성 화면");
+    return Container(
+      height: MediaQuery.of(context).size.height - 50,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'assets/images/prolog_new_project_background_image.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 65,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Text(
+              "Shake or Press Button \nto Watch New Photomasaic!",
+              style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 250,
+              width: MediaQuery.of(context).size.width - 100,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: FileImage(photomosaicImage), fit: BoxFit.contain)),
+            ),
+            SizedBox(
+              height: 55,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                //Get.to();
+              },
+              icon: Icon(
+                Icons.photo_library,
+              ),
+              style: ElevatedButton.styleFrom(primary: kHotpink),
+              label: Text("Using User Image"),
+            ),
+          ]),
+    );
   }
 
   @override
