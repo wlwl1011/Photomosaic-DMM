0aba255174629172d5e4d808d5d633e6aaee0acf
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-04-28 14:27:39 +0900

    make edit page

--- front/lib/screens/history/userHistory/components/my_info_edit.dart
+++ front/lib/screens/history/userHistory/components/my_info_edit.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:front/screens/history/otherHistory/components/post_widget.dart';
+import 'package:front/screens/history/userHistory/components/radial_progress.dart';
+import 'package:front/screens/history/userHistory/components/rounded_image.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
-class MyInfoEdit extends StatelessWidget {
+class MyInfoEdit extends StatefulWidget {
   const MyInfoEdit({Key? key}) : super(key: key);
 
   @override
+  State<MyInfoEdit> createState() => _MyInfoEditState();
+}
+
+class _MyInfoEditState extends State<MyInfoEdit> {
+  Widget _postList() {
+    return Column(
+      children: List.generate(50, (index) => PostWidget()).toList(),
+    );
+  }
+
+  var albumImage;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    // TODO: implement build
+
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('Edit profile',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w100)),
+          actions: <Widget>[
+            IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/ok.svg",
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(16.5),
+              onPressed: () {
+                //
+              },
+            )
+          ],
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: Get.height * 0.1,
+            ),
+            const RoundedImage(
+              imagePath: "assets/images/profile.jpeg",
+              size: Size.fromWidth(70.0),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  child: const Text("Chanege profile photo",
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold)),
+                  onTap: () async {
+                    var picker = ImagePicker();
+                    var image =
+                        await picker.pickImage(source: ImageSource.gallery);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
