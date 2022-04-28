dadc1696df6b7965165da95b05f3df99221aa59d
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-04-28 23:35:02 +0900

    done comment page

---
+++ front/lib/screens/history/otherHistory/components/message.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("");
+//DB 연결 후 수정
+    /*StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('time', descending: true)
+          .snapshots(),
+      builder: (context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = snapshot.data!.docs;
+
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (context, index) {
+            return Text(chatDocs[index]['text']);
+          },
+        );
+      },
+    );*/
+  }
+}
---
+++ front/lib/screens/history/otherHistory/components/new_message.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({Key? key}) : super(key: key);
+
+  @override
+  _NewMessageState createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _controller = TextEditingController();
+  var _userEnterMessage = '';
+  void _sendMessage() {
+    /*FocusScope.of(context).unfocus();
+    FirebaseFirestore.instance
+        .collection('chat')
+        .add({'text': _userEnterMessage, 'time': Timestamp.now()});
+    _controller.clear();*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Add a comment...'),
+              onChanged: (value) {
+                setState(() {
+                  _userEnterMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _userEnterMessage.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(Icons.send),
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
