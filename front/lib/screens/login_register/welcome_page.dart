fa3532920f1b355adb857050c1ecdb3703552445
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-05-01 11:24:01 +0900

    push

--- front/lib/screens/login_register/welcome_page.dart
+++ front/lib/screens/login_register/welcome_page.dart
@@ -11,7 +11,7 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor_l,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -25,28 +25,12 @@ class WelcomePage extends StatelessWidget {
                         margin: const EdgeInsets.only(top: 50),
                         width: MediaQuery.of(context).size.width,
                         child: Image(
-                          image: AssetImage('assets/images/title1.png'),
+                          image: AssetImage('assets/images/1.jpg'),
                         ),
                       ),
                     ),
                     SizedBox(
                       height: 20,
-                    ),
-                    Text(
-                      "Photomosaic",
-                      style: kHeadline,
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: Text(
-                        "Use our service to create unique photomosaic photos. \n You can experience unique events.",
-                        style: kBodyText,
-                        textAlign: TextAlign.center,
-                      ),
                     ),
                   ],
                 ),
@@ -89,41 +73,36 @@ class WelcomePage extends StatelessWidget {
                 margin: const EdgeInsets.only(bottom: 20),
                 child: Text(
                   "Or",
-                  style: TextStyle(fontSize: 15, color: Colors.white60),
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                   textAlign: TextAlign.center,
                 ),
               ),
-              Container(
-                // margin: const EdgeInsets.only(bottom: 50),
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.grey[850],
-                  borderRadius: BorderRadius.circular(18),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: MyTextButton(
-                        bgColor: Colors.white,
-                        buttonName: 'Register',
-                        onTap: () {
-                          Get.to(() => RegisterPage());
-                        },
-                        textColor: Colors.black87,
-                      ),
+              Row(
+                children: [
+                  Expanded(
+                    child: MyTextButton(
+                      bgColor: kHotpink,
+                      buttonName: 'Register',
+                      onTap: () {
+                        Get.to(() => RegisterPage());
+                      },
+                      textColor: Colors.white,
                     ),
-                    Expanded(
-                      child: MyTextButton(
-                        bgColor: Colors.transparent,
-                        buttonName: 'Sign In',
-                        onTap: () {
-                          Get.to(() => SignInPage());
-                        },
-                        textColor: Colors.white,
-                      ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: MyTextButton(
+                      bgColor: Colors.black,
+                      buttonName: 'Sign In',
+                      onTap: () {
+                        Get.to(() => SignInPage());
+                      },
+                      textColor: Colors.white,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
