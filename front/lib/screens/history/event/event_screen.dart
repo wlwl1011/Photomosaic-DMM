79514df3ee035a30f7cd26b0c5ebcd9b1e031716
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-05-02 14:58:34 +0900

    add more detail in event page

--- front/lib/screens/history/event/event_screen.dart
+++ front/lib/screens/history/event/event_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class eventScreen extends StatelessWidget {
   const eventScreen({Key? key}) : super(key: key);
@@ -63,22 +64,116 @@ class eventScreen extends StatelessWidget {
               color: Colors.black12,
               alignment: Alignment.centerLeft,
             ),
-            Container(
-              // constraints: BoxConstraints(maxHeight: 180, maxWidth: 180),
-              child: Image.asset(
-                'assets/images/too_cool.jpeg',
-                fit: BoxFit.contain,
-              ),
-            ),
-            Container(
-              // constraints: BoxConstraints(maxHeight: 180, maxWidth: 180),
-              child: Image.asset(
-                'assets/images/too_cool_logo.png',
-                fit: BoxFit.contain,
-              ),
+            Image.asset(
+              'assets/images/too_cool.jpeg',
+              fit: BoxFit.contain,
             ),
             SizedBox(
-              height: 10,
+              height: Get.height * 0.05,
+            ),
+            Container(
+              child: Column(
+                children: [
+                  const Text('PHOTOMOSAIC',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.left),
+                  SizedBox(
+                    height: Get.height * 0.01,
+                  ),
+                  const Text(
+                    'X TOO COLL FOR SCHOOL',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.05,
+                  ),
+                  const Text(
+                    '이벤트 기간',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.005,
+                  ),
+                  const Text(
+                    '4/25(월) ~ 5/02(월)',
+                    style: TextStyle(
+                      color: Colors.black45,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.05,
+                  ),
+                  const Text(
+                    '이벤트 경품',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.01,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(60, 20, 60, 20),
+                    child: Image.asset(
+                      'assets/images/event_m.jpg',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.05,
+                  ),
+                  const Text(
+                    '참여 방법',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.01,
+                  ),
+                  const Text(
+                    '1. 나만의 포토모자이크 사진 생성 (주제: 뷰티)',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.01,
+                  ),
+                  const Text(
+                    '2. 게시물에 #TooCooForSchool 포함하여 업로드',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+            ),
+            SizedBox(
+              height: Get.height * 0.05,
             ),
             Container(
               // constraints: BoxConstraints(maxHeight: 180, maxWidth: 180),
@@ -87,12 +182,19 @@ class eventScreen extends StatelessWidget {
                 fit: BoxFit.contain,
               ),
             ),
-            Container(
-              // constraints: BoxConstraints(maxHeight: 180, maxWidth: 180),
-              child: Image.asset(
-                'assets/images/too_for_school.jpg',
-                fit: BoxFit.contain,
-              ),
+            SizedBox(
+              height: Get.height * 0.05,
+            ),
+            Image.asset(
+              'assets/images/too_cool_logo.png',
+              fit: BoxFit.contain,
+            ),
+            SizedBox(
+              height: Get.height * 0.05,
+            ),
+            Image.asset(
+              'assets/images/too_for_school.jpg',
+              fit: BoxFit.contain,
             ),
           ],
         ),
